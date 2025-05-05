6ae6ea579b8292473d2dded389d7595c084fada3
unknown <villordomarco2020@gmail.com>
2025-05-05 16:48:17 -0300

    primer commit con .gitignore

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogicaDeBonoResultados;
+using LogicaDePresentismo;
+using LogicaDeSueldo;
+
+namespace GuiaObj_SueldoDePepe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado Pepe = new Empleado();
+            Pepe.Categoria = "Gerente";
+            Pepe.CantFaltas = 0;
+            Pepe.GetResultadoPresentismo = new BonoPresentismo();
+            Pepe.GetBonoResultados = new BonoResultados.Bono10Porciento();
+
+            Console.WriteLine("Sueldo de Pepe: $" + Pepe.CalcularSueldo());
+        }
+    }
+}
